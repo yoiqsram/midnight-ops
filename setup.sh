d438e6bccfc063aacf7d3c0ec3b28c35a27f0602
yoiqsram <yoiqrambadian@gmail.com>
2024-03-01 01:00:52 +0700

    add tensorflow-gpu to environment

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 conda env create --prefix ./venv -f environment.yml
 conda activate ./venv
-conda config --set env_prompt '(./venv) '
-
-pip install git+https://github.com/yoiqsram/leantask.git@7f286fb
+conda config --set env_prompt '({name}) '
 
 kaggle competitions download -c ml-olympiad-tfugsurabaya-2024
 unzip ml-olympiad-tfugsurabaya-2024.zip -d data
 rm ml-olympiad-tfugsurabaya-2024.zip
+
+pip install git+https://github.com/yoiqsram/leantask.git@7f286fb
 
 conda deactivate
