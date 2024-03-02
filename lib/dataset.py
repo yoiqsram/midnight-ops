8663e51ccb972953e8756b8bccad426bb8018a79
yoiqsram <yoiqrambadian@gmail.com>
2024-03-03 02:40:56 +0700

    reshape dataset output and add notebook preprocess, classifier, to submission file generation

--- lib/dataset.py
+++ lib/dataset.py
@@ -5,13 +5,13 @@ from .constants import PROJECT_DIR, EXPERIMENT_NAME
 __all__ = ['load_train_data', 'load_test_data']
 
 
-def load_train_data() -> pd.DataFrame:
+def load_train_data() -> tuple[list[str], list[int]]:
     file_path = PROJECT_DIR / 'data' / EXPERIMENT_NAME / 'train.tsv'
     data = pd.read_table(file_path)
-    return data
+    return data.REVIEW.values.tolist(), data.LABEL.values.tolist()
 
 
-def load_test_data() -> pd.DataFrame:
+def load_test_data() -> list[str]:
     file_path = PROJECT_DIR / 'data' / EXPERIMENT_NAME / 'test.tsv'
     data = pd.read_table(file_path)
-    return data
+    return data.REVIEW.values.tolist()
--- lib/sklearn/preprocess/nlp.py
+++ lib/sklearn/preprocess/nlp.py
@@ -1,9 +1,9 @@
 import nltk
-import numpy as np
 import pandas as pd
+import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from sklearn.base import TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 from lib.constants import PROJECT_DIR
@@ -12,87 +12,147 @@ nltk.download('punkt', quiet=True)
 nltk.download('words', quiet=True)
 nltk.download('stopwords', quiet=True)
 
-
-class TextTokenizer(TransformerMixin):
-    def __init__(self, column: str) -> None:
-        self.column = column
-
-    def transform(self, X: pd.DataFrame, **kwargs):
-        return X.assign(**{
-            self.column: X[self.column].str.lower().map(word_tokenize)
-        })
+INDONESIAN_ROOT_WORDS = set()
+INDONESIAN_INFORMAL_TO_FORMAL_MAPPER = dict()
 
 
-class WordsMapper(TransformerMixin):
-    def __init__(self, column: str, mapper: dict[str, str]) -> None:
-        self.column = column
+def flatten_tokens(tokens: list[str]) -> list[str]:
+    _tokens = []
+    for token in tokens:
+        if ' ' in token:
+            _tokens += token.split()
+        else:
+            _tokens.append(token)
+
+    return _tokens
+
+
+class TextTokenizer(BaseEstimator, TransformerMixin):
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: list[str], **kwargs):
+        return [word_tokenize(text) for text in X]
+
+
+class WordsMapper(BaseEstimator, TransformerMixin):
+    def __init__(self, mapper: dict[str, str]) -> None:
         self.mapper = mapper
 
-    def transform(self, X: pd.DataFrame, **kwargs):
-        return X.assign(**{
-            self.column: X[self.column].map(lambda words: [self.mapper.get(word, word) for word in words])
-        })
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: list[list[str]], **kwargs):
+        X_mapped = [
+            [self.mapper.get(word, word) for word in words]
+            for words in X
+        ]
+        return [flatten_tokens(words) for words in X_mapped]
 
 
 class WordsFormalizer(WordsMapper):
-    def __init__(self, column: str) -> None:
-        collex = pd.read_table(
-            PROJECT_DIR
-            / 'data' / 'indo-collex'
-            / 'informal-to-formal-dictionary.tsv'
-        )
-        mapper = {
-            row['informal']: row['formal']
-            for _, row in collex.iterrows()
-        }
+    def __init__(self) -> None:
+        global INDONESIAN_INFORMAL_TO_FORMAL_MAPPER
 
-        super(WordsFormalizer, self).__init__(column, mapper)
+        if len(INDONESIAN_INFORMAL_TO_FORMAL_MAPPER) == 0:
+            collex = pd.read_table(
+                PROJECT_DIR
+                / 'data' / 'indo-collex'
+                / 'informal-to-formal-dictionary.tsv'
+            )
+            INDONESIAN_INFORMAL_TO_FORMAL_MAPPER = {
+                row['informal']: row['formal']
+                for _, row in collex.iterrows()
+            }
+
+        super(WordsFormalizer, self).__init__(INDONESIAN_INFORMAL_TO_FORMAL_MAPPER)
 
 
-class WordsFilter(TransformerMixin):
-    def __init__(self, column: str, words: set[str], exclusive: bool = True) -> None:
-        self.column = column
+class WordsFilter(BaseEstimator, TransformerMixin):
+    def __init__(self, words: set[str], exclusive: bool = True) -> None:
         self.words = words
         self.exclusive = exclusive
 
-    def transform(self, X: pd.DataFrame, **kwargs):
-        if self.exclusive:
-            return X.assign(**{
-                self.column: X[self.column].map(lambda words: [word for word in words if word not in self.words])
-            })
+    def fit(self, X, y=None):
+        return self
 
-        return X.assign(**{
-            self.column: X[self.column].map(lambda words: [word for word in words if word in self.words])
-        })
+    def transform(self, X: list[list[str]], **kwargs):
+        if self.exclusive:
+            return [
+                [word for word in words if word not in self.words]
+                for words in X
+            ]
+
+        return [
+            [word for word in words if word in self.words]
+            for words in X
+        ]
 
 
 class StopWordsFilter(WordsFilter):
-    def __init__(self, column: str) -> None:
+    def __init__(self) -> None:
         stop_words = set(stopwords.words('indonesian'))
 
-        super(StopWordsFilter, self).__init__(column, stop_words)
+        super(StopWordsFilter, self).__init__(stop_words)
 
 
 class UnknownWordsFilter(WordsFilter):
-    def __init__(self, column: str) -> None:
-        with open(PROJECT_DIR / 'data' / 'sastrawi' / 'kata-dasar.txt') as f:
-            known_words = set(f.read().splitlines())
-        
-        super(UnknownWordsFilter, self).__init__(column, known_words, False)
+    def __init__(self) -> None:
+        global INDONESIAN_ROOT_WORDS
+
+        if len(INDONESIAN_ROOT_WORDS) == 0:
+            with open(PROJECT_DIR / 'data' / 'sastrawi' / 'kata-dasar.txt') as f:
+                INDONESIAN_ROOT_WORDS = set(f.read().splitlines())
+
+        super(UnknownWordsFilter, self).__init__(INDONESIAN_ROOT_WORDS, False)
 
 
-class WordsLemmatization(TransformerMixin):
-    def __init__(self, column: str) -> None:
-        self.column = column
+class SpecialCharacterFilter(BaseEstimator, TransformerMixin):
+    def __init__(self) -> None:
+        self.pattern = r'[\W_]+'
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: list[list[str]], **kwargs):
+        return [
+            [
+                word for word in words
+                if len(word) > 0 and re.match(self.pattern, word) is None
+            ]
+            for words in X
+        ]
+
+
+class WordsLemmatization(BaseEstimator, TransformerMixin):
+    def __init__(self) -> None:
         self.stemmer = StemmerFactory().create_stemmer()
 
-    def transform(self, X: np.array, **kwargs):
-        return X.assign(**{
-            self.column: X[self.column].map(lambda words: [self.stemmer.stem(word) for word in words])
-        })
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: list[list[str]], **kwargs):
+        return [
+            [self.stemmer.stem(word) for word in words]
+            for words in X
+        ]
 
 
-def extract_unknown_words(words: np.array):
-    known_words = UnknownWordsFilter('').words
+class TokenToTextTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: list[list[str]], **kwargs):
+        return [' '.join(words) for words in X]
+
+
+def extract_unknown_words(words: list[str]):
+    known_words = UnknownWordsFilter().words
     unknown_words = [word for word in words if word not in known_words]
     return unknown_words
