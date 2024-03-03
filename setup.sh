5c1288a9e65d7112f52b9fe618d74ce45172e20b
yoiqsram <yoiqrambadian@gmail.com>
2024-03-03 21:32:43 +0700

    adjust environment setup to be working in Windows bash

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ PYTHONPATH="$(dirname "$SCRIPT_PATH")"
 COMPETITION="ml-olympiad-tfugsurabaya-2024"
 
 if [ "$1" == "--minimal" ]; then
-    conda env create --prefix ./venv -f environment.minimal.yml
+    conda create --prefix ./venv -f jupyterlab pandas nltk mlflow
 elif [ "$1" == "" ]; then
-    conda env create --prefix ./venv -f environment.yml
+    conda create --prefix ./venv -f jupyterlab pandas nltk mlflow tensorflow-gpu
 else
     echo "Failed to setup environment."
     exit 1
@@ -17,6 +17,7 @@ conda activate ./venv
 conda config --set env_prompt '({name}) '
 conda env config vars set PYTHONPATH=$PYTHONPATH
 
+pip install kaggle
 kaggle competitions download -c $COMPETITION
 unzip $COMPETITION.zip -d data/$COMPETITION
 rm $COMPETITION.zip
